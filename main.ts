03e9a4c15c37da3edd8d7bea1dba5faef47c4700
iAndrrei <97289654+iAndrrei@users.noreply.github.com>
2022-05-13 11:59:47 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,3 +13,14 @@ basic.forever(function () {
         cuteBot.motors(-30, -30)
     }
 })
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(75, 25)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(25, 75)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(50, 50)
+    }
+})
